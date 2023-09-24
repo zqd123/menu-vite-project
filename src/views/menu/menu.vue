d9abf245faea4d8d7435d4c95275c5e4fbd62ef7
张全地 <zhangqdmacmini@zhangquandideMac-mini.local>
2023-09-24 11:03:00 +0800

    feat: 方向控制

--- src/views/menu/menu.vue
+++ src/views/menu/menu.vue
@@ -1,34 +1,74 @@
 <template>
-  <div class="flex h-full w-full">
-    <el-switch
-    class="!absolute right-0 top-0"
-    v-model="isHide"
-    @change="refreshStatus"
-    inline-prompt
-    active-text="隐藏"
-    inactive-text="展示"
-  />
+  <div class="flex h-full w-full" :class="isGlobalColumn?'flex-col':'flex-row'">
+    <div class="flex !absolute right-0 top-0">
+      <el-switch
+        v-model="isHide"
+        @change="refreshStatus"
+        inline-prompt
+        active-text="隐藏"
+        inactive-text="展示"
+      />
+      <el-switch
+        v-model="isThirdColumn"
+        inline-prompt
+        active-text="三级横向"
+        inactive-text="三级竖向"
+      />
+      <el-switch
+        v-model="isGlobalColumn"
+        inline-prompt
+        active-text="横向"
+        inactive-text="竖向"
+      />
+    </div>
     <transition name="fade">
-      <div v-show="isShowFirstMenu" class="bg-blue-950 flex flex-col gap-4 p-4 text-white">
-        <div v-for="item in menu1" :key="item.id" @click="firstMenuClick">
+      <div
+        v-show="isShowFirstMenu"
+        class="bg-blue-950 flex  gap-4 p-4 text-white"
+        :class="!isGlobalColumn ? 'flex-col' : 'flex-row justify-center'"
+      >
+        <div
+          v-for="item in menu1"
+          :key="item.id"
+          @click="firstMenuClick"
+          class="font-bold"
+        >
           {{ item.name }}
         </div>
       </div>
     </transition>
 
     <transition name="fade">
-      <div v-show="isShowSecondMenu" class="bg-stone-300 flex flex-col gap-4 p-4 text-black">
-        <div v-for="item in menu2" :key="item.id" @click="secondMenuClick" >
+      <div
+        v-show="isShowSecondMenu"
+        class="bg-stone-300 flex gap-4 p-4 text-black"
+        :class="!isGlobalColumn ? 'flex-col' : 'flex-row justify-center'"
+      >
+        <div
+          v-for="item in menu2"
+          :key="item.id"
+          @click="secondMenuClick"
+          class="font-bold"
+        >
           {{ item.name }}
         </div>
       </div>
     </transition>
 
     <transition name="fade">
-      <div v-show="isShowThirdMenu" class="flex-1 bg-white flex flex-col gap-4 p-4 text-black">
-        <div v-for="item in menu3" :key="item.id" class="flex items-center gap-4">
-          <span>{{ item.name }}</span>
-          <div class="flex gap-4">
+      <div
+        v-show="isShowThirdMenu"
+        class="flex-1 bg-white flex  gap-4 p-4 text-black"
+        :class="thirdCenter"
+      >
+        <div
+          v-for="item in menu3"
+          :key="item.id"
+          class="flex items-center gap-4 "
+          :class="!isThirdColumn ? 'flex-row' : 'flex-col'"
+        >
+          <span class="font-bold">{{ item.name }}</span>
+          <div class="flex gap-4" :class="!isThirdColumn ? 'flex-row' : 'flex-col'">
             <div v-for="item in menuChild" :key="item.id" @click="selectClick">
               {{ item.name }}
             </div>
@@ -40,56 +80,66 @@
 </template>
 <script lang="ts" setup>
 import { ElMessageBox } from "element-plus";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import { menu1, menu2, menu3, menuChild } from "./params";
-const isHide = ref(true)// 展开下级菜单,隐藏上级菜单
-const isShowFirstMenu = ref(true)
-const isShowSecondMenu = ref(false)
-const isShowThirdMenu = ref(false)
+const isHide = ref(false); // 展开下级菜单,隐藏上级菜单
+const isGlobalColumn = ref(false);// 菜单全局方向
+const isThirdColumn = ref(false); // 三级菜单方向
+const isShowFirstMenu = ref(true);
+const isShowSecondMenu = ref(false);
+const isShowThirdMenu = ref(false);
+const thirdCenter = computed(()=>{
+  if (isThirdColumn.value) {
+    return 'flex-row ' + `${isGlobalColumn.value ? 'justify-center':'justify-start'}`
+  } else {
+    return 'flex-col '+ `${isGlobalColumn.value ? 'items-center':'items-start'}`
+    
+  } 
+})
 
-const initTime = new Date();// 初始时间
+const initTime = new Date(); // 初始时间
 
 //一级菜单
-const firstMenuClick = ()=>{
-  console.log('一级菜单点击');
-  if(isHide.value){
-    isShowFirstMenu.value = false
+const firstMenuClick = () => {
+  console.log("一级菜单点击");
+  if (isHide.value) {
+    isShowFirstMenu.value = false;
   }
-  isShowSecondMenu.value =true
+  isShowSecondMenu.value = true;
   // isShowSecondMenu.value =!isShowSecondMenu.value
-}
+};
 
 // 二级菜单
-const secondMenuClick = ()=>{
-  if(isHide.value){
-    isShowFirstMenu.value = false
-    isShowSecondMenu.value = false
+const secondMenuClick = () => {
+  if (isHide.value) {
+    isShowFirstMenu.value = false;
+    isShowSecondMenu.value = false;
   }
-  isShowThirdMenu.value = true
-}
+  isShowThirdMenu.value = true;
+};
 
 //选中
-const selectClick = ()=>{
-  console.log('选中');
+const selectClick = () => {
+  console.log("选中");
   const nowTime = new Date();
-  const countTime = (nowTime.getTime() - initTime.getTime())/1000
-  ElMessageBox.alert(`用时: ${countTime}秒`, '提示', {
-    confirmButtonText: '确定',
-    callback:()=>{
-      refreshStatus()
-    }
-  })
-  
-}
+  const countTime = (nowTime.getTime() - initTime.getTime()) / 1000;
+  ElMessageBox.alert(`用时: ${countTime}秒`, "提示", {
+    confirmButtonText: "确定",
+    callback: () => {
+      refreshStatus();
+    },
+  });
+};
 //重置刷新
-const refreshStatus = ()=>{
-  isShowFirstMenu.value = true
-  isShowSecondMenu.value =false
-  isShowThirdMenu.value = false
-}
+const refreshStatus = () => {
+  isShowFirstMenu.value = true;
+  isShowSecondMenu.value = false;
+  isShowThirdMenu.value = false;
+};
 </script>
 <style scoped>
-.fade-enter-active, .fade-leave-active {
+.fade-enter-active,
+.fade-leave-active {
   transition: opacity 0.3s ease;
 }
 .fade-enter-from,
