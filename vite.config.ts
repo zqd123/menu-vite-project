4354dab9d1f1ac480f06009dd0182041c4ffa3c1
张全地 <zhangqdmacmini@zhangquandideMac-mini.local>
2023-09-24 18:00:01 +0800

    打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,30 @@ import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import ElementPlus from "unplugin-element-plus/dist/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./", //静态资源访问路径（部署）
+  build: {
+    rollupOptions: {
+      output: {
+        // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+        sanitizeFileName(name) {
+          const match = DRIVE_LETTER_REGEX.exec(name);
+          const driveLetter = match ? match[0] : "";
+          // A `:` is only allowed as part of a windows drive letter (ex: C:\foo)
+          // Otherwise, avoid them because they can refer to NTFS alternate data streams.
+          return (
+            driveLetter +
+            name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
+          );
+        },
+      },
+    },
+  },
   plugins: [
     vue(),
     ElementPlus({
