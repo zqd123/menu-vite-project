d336a9d81c909e8f4b1462124b3dbae5d59968e9
张全地 <zhangqdmacmini@zhangquandideMac-mini.local>
2023-11-02 21:46:17 +0800

    测试

--- src/views/menu/useProcessDataHook.ts
+++ src/views/menu/useProcessDataHook.ts
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 import { ref } from "vue";
-import stringData from './stringData.txt'
+import stringData from './stringData1.txt'
  export interface Menu {
   menuId: string;
   menuName: string;
